7b8a1e114df0d0541eb82350f9bb4772b88057f9
Namit <namityadav008@gmail.com>
2023-09-27 14:59:18 +0530

    updates

---
+++ C++_Class/Operator Overloading .cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+int main(){
+
+
+
+    //we can use all c++ operators except 
+    //scope , * , sizeof
+    // no of arg depends upon the operator to be unary or binary 
+    
+
+}
