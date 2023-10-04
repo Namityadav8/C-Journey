451213add38b8b3ecd8477ecc63cd46bffe2900a
Namit <namityadav008@gmail.com>
2023-10-04 14:23:15 +0530

    updates

--- C++_Class/Operator Overloading .cpp
+++ C++_Class/Operator Overloading .cpp
@@ -1,12 +1,17 @@
-#include<iostream>
-using namespace std ;
-int main(){
+// #include<iostream>
+// using namespace std ;
+// int main(){
 
 
 
-    //we can use all c++ operators except 
-    //scope , * , sizeof
-    // no of arg depends upon the operator to be unary or binary 
+ 
+//  void operator -(invert_position &i){
+//     i.x = i.y;
+//     i.y = i.x;
     
+//  }
+//  int main(){
 
-}
+//  }
+
+// }
