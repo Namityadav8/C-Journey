599fbaa63869343eb2cd0e145ca310252e404340
Namit <namityadav008@gmail.com>
2023-10-06 14:29:47 +0530

    updates

---
+++ C++_Class/Post and Prefix .cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std; 
+class uf{
+    int a , b;
+    int c;
+    public:
+    void getvalues(){
+        cout<<"Values of a, b ,c ";
+        cin>>a>>b>>c;
+    }
+    void show(){
+        cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<endl;
+
+    }
+    void friend operator--(uf &x,int x1);
+};
+void operator--(uf &x,int y){
+    x.a=x.a--;
+    x.b=x.b--;
+    x.c=x.c--;
+}
+int main(){
+    uf x1;
+    x1.getvalues();
+    cout<<"Before Overloading\n";
+    x1.show();
+    cout<<"After Overloading \n";
+} 
