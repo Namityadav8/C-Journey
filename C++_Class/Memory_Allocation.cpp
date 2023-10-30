57a0609ad2197fe2fdc5ee09546560d6bad23f56
Namit <namityadav008@gmail.com>
2023-10-31 00:07:35 +0530

    changes

--- C++_Class/Memory_Allocation.cpp
+++ C++_Class/Memory_Allocation.cpp
@@ -1,5 +1,27 @@
 #include<iostream>
-using namespace std; 
+using namespace std;
+
+class base{
+    public:
+    base(){
+        cout<<"Constructing base\n";
+    }
+    ~base(){
+        cout<<"Destructing base\n";
+    }
+};
+
+class derived1:public base{
+    public:
+    derived1(){
+        cout<<"Constructing derived\n";
+    }
+    ~derived1(){
+        cout<<"Destructing derived\n";
+    };
+};
 int main(){
-    
-} 
+base *b=new derived1;
+delete b;
+return 0;
+}
